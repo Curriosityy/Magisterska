c80a69ef523347aae9d1e0e3a6253bf992005cc7
Curriosityy <patryk9595@gmail.com>
2020-01-02 22:18:05 +0100

    +added simple getter to get input value from textbox

---
+++ Assets/InputCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class InputCommand : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI _output;
+    /*
+     * To consider if output window is needed.
+     */
+    private TMP_InputField _inputField;
+
+    private void Start()
+    {
+        _inputField = GetComponent<TMP_InputField>();
+    }
+
+    public string GetInputValue()
+    {
+        /*
+         *                              TODO:
+         * some kind of parser, depends on what we want to do with commands.
+         */
+        string inputToReturn = _inputField.text;
+        _inputField.text = "";
+        return _inputField.text;
+    }
+}
