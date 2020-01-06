d3ce01c3b812aafad2670340a5524cc7c638c350
Curriosityy <patryk9595@gmail.com>
2020-01-06 11:47:02 +0100

    +added minion script where we handle dmg

---
+++ Assets/Minion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour,IDamageable
+{
+    [SerializeField] private int _maxHealth;
+    private int _health;
+    public void DealDamage(int damagage)
+    {
+        _health -= damagage;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = _maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
