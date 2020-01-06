a095c42efec67db26d00b08407a229fbb9367aa0
Curriosityy <patryk9595@gmail.com>
2020-01-06 15:08:08 +0100

    -renamed Minion in to MinionHealth -Created empty minion class.

--- Assets/Minion.cs
+++ Assets/Minion.cs
@@ -1,24 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class Minion : BarStatistic, IDamageable
+public class Minion : MonoBehaviour
 {
    
-    public void DealDamage(int damagage)
-    {
-        var oldHp = _statistic;
-        _statistic -= damagage;
-        CalculateBar();
-        SpawnText(damagage);
-        if (_statistic <= 0)
-        {
-            Die();
-        }
-        Debug.Log(string.Format("{0} Took {1} DMG, HP Before {2} HP now {3}", gameObject.name, damagage, oldHp, _statistic));
-    }
-
-    private void Die()
-    {
-        //throw new NotImplementedException();
-    }
+   
 }
---
+++ Assets/MinionHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionHealth : BarStatistic, IDamageable
+{
+    public void DealDamage(int damagage)
+    {
+        var oldHp = _statistic;
+        _statistic -= damagage;
+        CalculateBar();
+        SpawnText(damagage);
+        if (_statistic <= 0)
+        {
+            Die();
+        }
+        Debug.Log(string.Format("{0} Took {1} DMG, HP Before {2} HP now {3}", gameObject.name, damagage, oldHp, _statistic));
+    }
+
+    private void Die()
+    {
+        //throw new NotImplementedException();
+    }
+}
