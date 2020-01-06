4e0fa9b53cc3d41e316c177eaa6e2b1ce689421f
Curriosityy <patryk9595@gmail.com>
2020-01-06 15:41:50 +0100

    =changed access modifiers, and added field text color

--- Assets/BarStatistic.cs
+++ Assets/BarStatistic.cs
@@ -5,11 +5,12 @@ using TMPro;
 using UnityEngine.UI;
 public abstract class BarStatistic : MonoBehaviour
 {
-    [SerializeField] protected int _maxStat = 100;
-    [SerializeField] protected Image _statBar;
-    [SerializeField] protected TextMeshProUGUI _text;
-    [SerializeField] protected Canvas _minionCanvas;
-    protected int _statistic;
+    [SerializeField] protected int maxStat = 100;
+    [SerializeField] private Image _statBar;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private Canvas _minionCanvas;
+    [SerializeField] private Color _textColor;
+    protected int statistic;
     // Start is called before the first frame update
     protected void SpawnText(int damage)
     {
@@ -17,17 +18,19 @@ public abstract class BarStatistic : MonoBehaviour
         dmgText.transform.SetParent(_minionCanvas.transform);
         dmgText.transform.localPosition = new Vector3(Random.Range(-0.1f, 0.1f), 0, 0);
         dmgText.text = damage.ToString();
+
         Destroy(dmgText.gameObject, dmgText.GetComponent<Animation>().clip.length);
     }
 
     protected void CalculateBar()
     {
-        _statBar.fillAmount = (_statistic * 1.0f) / _maxStat;
+        _statBar.fillAmount = (statistic * 1.0f) / maxStat;
     }
     protected virtual void Start()
     {
-        Debug.Log("test1");
-        _statistic = _maxStat;
+        _text.color = _textColor;
+        statistic = maxStat;
         CalculateBar();
+        
     }
 }
