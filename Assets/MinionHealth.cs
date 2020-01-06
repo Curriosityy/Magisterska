a364f5092b734fa6a5242f87245d5df13e9fb3e9
Curriosityy <patryk9595@gmail.com>
2020-01-06 15:42:10 +0100

    no message

--- Assets/MinionHealth.cs
+++ Assets/MinionHealth.cs
@@ -6,15 +6,15 @@ public class MinionHealth : BarStatistic, IDamageable
 {
     public void DealDamage(int damagage)
     {
-        var oldHp = _statistic;
-        _statistic -= damagage;
+        var oldHp = statistic;
+        statistic -= damagage;
         CalculateBar();
         SpawnText(damagage);
-        if (_statistic <= 0)
+        if (statistic <= 0)
         {
             Die();
         }
-        Debug.Log(string.Format("{0} Took {1} DMG, HP Before {2} HP now {3}", gameObject.name, damagage, oldHp, _statistic));
+        Debug.Log(string.Format("{0} Took {1} DMG, HP Before {2} HP now {3}", gameObject.name, damagage, oldHp, statistic));
     }
 
     private void Die()
