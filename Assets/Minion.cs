f27f66d262f15ab11b911432f29c3875eae785fa
Curriosityy <patryk9595@gmail.com>
2020-01-06 12:03:16 +0100

    +added debug log

--- Assets/Minion.cs
+++ Assets/Minion.cs
@@ -8,7 +8,9 @@ public class Minion : MonoBehaviour,IDamageable
     private int _health;
     public void DealDamage(int damagage)
     {
+        var oldHp = _health;
         _health -= damagage;
+        Debug.Log(string.Format("{0} Took {1} DMG, HP Before {2} HP now {3}", gameObject.name, damagage, oldHp, _health));
     }
 
     // Start is called before the first frame update
