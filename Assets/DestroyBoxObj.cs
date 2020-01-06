f5d657631fa408ce5a5f035a9d52294bc650e71e
lucas <mrmqs159@gmail.com>
2020-01-06 12:48:46 +0100

    Obstacles
    Added destroyable obstacles

---
+++ Assets/DestroyBoxObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBoxObj : MonoBehaviour, IDamageable
+{
+    
+    public GameObject destroyedV;
+       
+    
+    public void DealDamage(int damagage)
+    {
+        Instantiate(destroyedV, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+    }
+}
