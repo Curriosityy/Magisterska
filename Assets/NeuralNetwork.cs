41899887617837fc7a8c51d5644bd444266bae65
lucas <mrmqs159@gmail.com>
2020-03-28 23:39:57 +0100

    comparingGenomes
    Added comparing methods

--- Assets/NeuralNetwork.cs
+++ Assets/NeuralNetwork.cs
@@ -61,7 +61,88 @@ public class NeuralNetwork
     {
         return _neurons.Where(n => n.Type == type).ToList();
     }
+    
+    public bool Compare(NeuralNetwork neatToCompare)
+    {
+        bool excessJoin = true;
+        int excessJoins = 0;
+        int disJoins = 0;
+        int edgesCount = 0;
+        Double weightDiff = 0f;
+        NeuralNetwork neat1;
+        NeuralNetwork neat2;
+        if (Connection.Count >= neatToCompare.Connection.Count)
+        {
+            neat1 = this;
+            neat2 = neatToCompare;
+        }
+        else
+        {
+            neat1 = neatToCompare;
+            neat2 = this;
+        }
 
+        edgesCount = neat1.Connection.Count;
+        for (int i = edgesCount - 1; i > 0; i--)
+        {
+            if (neat2.DoesInnovNumberExist(/*Connection[i].innovnum*/ 1))
+            {
+                excessJoin = false;
+                int connID = neat2.getEgdeId(/*Connection[i].innovnum*/1);
+                weightDiff += Math.Abs(neat1.Connection[i].Weight-neat2.Connection[connID].Weight);
+            }
+            else
+            {
+                if (excessJoin == true)
+                {
+                    excessJoins += 1;
+                }
+                else
+                {
+                    disJoins += 1;
+                }
+            }
+        }
+        foreach(var diffCon in neat2.Connection)
+        {
+            if (!neat1.DoesInnovNumberExist(1 /*diffCon.Innovnum*/))
+            {
+                disJoins += 1;
+            }
+        }
+        float delta = (NeatValues.excessjoinsCoefficiant * excessJoins) / edgesCount + 
+            (NeatValues.disjoinsCoefficiant * disJoins) / edgesCount + NeatValues.weightCoefficiant * (float)weightDiff;
+        if (delta<NeatValues.simularityTreshhold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public int getEgdeId(int innovNum)
+    {
+        for(int i = 0; i < Connection.Count; i += 1)
+        {
+            if (/*Connection[i].innoNumber==innovNum*/ true)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+    private bool DoesInnovNumberExist(int innovNum)
+    {
+        foreach (var edge in Connection)
+        {
+            if (/*Connection.innoNumber == innovNum*/ true)//trzeba dodac innov number polaczenia
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     private bool IsEdgeExistInDatabase(int neuronFromId,int neuronToId)
     {
         return allEdges.Where(e => e.ConnectedFrom == neuronFromId && e.ConnectedTo == neuronToId).Count() == 1;
