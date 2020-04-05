8f84a921c9b43cb1c90631c30997f3207dacaa39
Curriosityy <patryk9595@gmail.com>
2020-04-05 14:39:39 +0200

    -changed values, todo repair bug with empty population

--- Assets/NeatValues.cs
+++ Assets/NeatValues.cs
@@ -32,28 +32,28 @@ public static class NeatValues {
     //public const float linearActivFunValue = 1f;
     //Speciation
     public const int maxStagnation=15;
-    public const float weightCoefficiant=0.6f;
+    public const float weightCoefficiant=0.5f;
     public const float disjoinsCoefficiant=1f;
     public const float excessjoinsCoefficiant=1f;
-    public const float survivingRate=0.4f;
+    public const float survivingRate=0.2f;
     public const int minSpieceSize=2;
-    public const float simularityTreshhold=.9f;
+    public const float simularityTreshhold=2f;
     //Genome
     public const float addConnProbability=0.5f;
     public const float removeConnProbability=0.2f;
     public const float changeConnStatusProbability=0.1f;
     public const float addNodeProbability=0.2f;
-    public const float removeNodeProbability=0.1f;
+    public const float removeNodeProbability=0.2f;
     //Node
-    public const float minBias=-1f;
-    public const float maxBias=1f;
-    public const float biasMutationProbability=0.3f;
+    public const float minBias=-30f;
+    public const float maxBias= 30f;
+    public const float biasMutationProbability=0.5f;
     public const float biasRandomMutationProbability=0.1f;
 
     //Connection
-    public const float minWeight=-1f;
-    public const float maxWeight=1f;
-    public const float weightMutationProbability=0.3f;
+    public const float minWeight=-30f;
+    public const float maxWeight=30f;
+    public const float weightMutationProbability=0.5f;
     public const float weightRandomMutationProbability=0.1f;
 
    
