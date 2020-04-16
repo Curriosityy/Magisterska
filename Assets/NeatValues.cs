6ec96d0e8359e4006a47722a4b9a2823073e1e86
Curriosityy <patryk9595@gmail.com>
2020-04-16 15:49:26 +0200

    no message

--- Assets/NeatValues.cs
+++ Assets/NeatValues.cs
@@ -38,10 +38,10 @@ public static class NeatValues {
     public const float elitismRate = 0.1f;
     public const float asexualReproductionProbability = 0.2f;
     public const int minSpieceSize=2;
-    public const float weightCoefficiant=0.5f;
-    public const float disjoinsCoefficiant=1f;
-    public const float excessjoinsCoefficiant=1f;
-    public const float simularityTreshhold=910f;
+    public const float weightCoefficiant=0.2f;
+    public const float disjoinsCoefficiant=0.5f;
+    public const float excessjoinsCoefficiant= 0.5f;
+    public const float simularityTreshhold=400f;
     //Genome
     public const float addConnProbability=0.4f;
     public const float removeConnProbability=0.2f;
